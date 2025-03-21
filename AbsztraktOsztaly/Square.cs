18842cb7dfa0b626d7eb775997259689b2e8d5ac
kormonditamas2222 <t.kormond2006@gmail.com>
2025-03-21 11:38:57 +0100

    square

---
+++ AbsztraktOsztaly/Square.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shape
+{
+    internal class Square : Rectangle
+    {
+        public double Height { get => Width; set => Height = value; }
+        public double Width { get => Height; set => Width = value; }
+        public Square(bool isHoley, string color, int height, int width) : base(isHoley, color, height, width)
+        {
+        }
+
+        public override string ToString()
+        {
+            return "Négyzet " + base.ToString();
+        }
+    }
+}
