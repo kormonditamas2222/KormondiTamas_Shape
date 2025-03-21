dbbd195ae088ec82d7b8772d1007fd89d9993103
kormonditamas2222 <t.kormond2006@gmail.com>
2025-03-21 11:18:45 +0100

    shape, rectangle

---
+++ AbsztraktOsztaly/Rectangle.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shape
+{
+    internal class Rectangle : Shape
+    {
+        double height;
+        double width;
+
+        public double Height { get => height; set => height = value; }
+        public double Width { get => width; set => width = value; }
+
+        public Rectangle(bool isHoley, string color, int height, int width) : base(isHoley, color) 
+        {
+            this.height = height;
+            this.width = width;
+        }
+        public override double Perimeter()
+        {
+            return 2 * (height + width);
+        }
+
+        public override double Area()
+        {
+            return height * width;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + " - téglalap";
+        }
+    }
+}
---
+++ AbsztraktOsztaly/Shape.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shape
+{
+    internal abstract class Shape
+    {
+        bool isHoley;
+        string color;
+
+        public string Color { get { return color; } set { color = value; } }
+        protected Shape(bool isHoley, string color) 
+        {
+            this.isHoley = isHoley;
+            this.color = color;
+        }
+        protected Shape(string color) 
+        {
+            this.color = color;
+            isHoley = false;
+        }
+        public void MakeHoley()
+        {
+            isHoley = true;
+        }
+        public abstract double Perimeter();
+        public abstract double Area();
+        public override string ToString() 
+        {
+            return $"{color} - {isHoley} : K: {Perimeter()} T: {Area()}";
+        }
+    }
+}
